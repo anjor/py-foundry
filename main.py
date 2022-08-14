dff056839443b0c5006266b80c6ec717895ac9f4
anjor <anjor@umd.edu>
2022-08-14 15:40:40 +0100

    example

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from foundry.services.phonograph2.table_storage_service import TableStorageServi
 multipass = Multipass("/Users/anjor/.py-foundry.json")
 phonograph = TableStorageService("/Users/anjor/.py-foundry.json")
 
-print(multipass.get_me())
-print(phonograph.get_events("ri.phonograph2.main.table.ac6ad9cf-78d1-482b-9d00-ed9c31ba0097"))
+#print(multipass.get_me())
+print(phonograph.get_events("ri.phonograph2.main.table.ac6ad9cf-78d1-482b-9d00-ed9c31ba0097", paging_token=5, page_size=2))
+print(phonograph.get_rows("ri.phonograph2.main.table.ac6ad9cf-78d1-482b-9d00-ed9c31ba0097", get_rows_request={"getRowsRequest":{"primaryKeys":[{"preference_id":"94e516bb-fd23-41fc-a8d4-f97cba06b50b"},{'preference_id': '045dc8f5-5682-486d-a618-9ad731477aaa'}]}}))
